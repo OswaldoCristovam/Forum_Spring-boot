b9692c1f07e9ea21ecc1dcb5a42edeb34adf7924
oswaldo <oswaldo.jose.c@hotmail.com>
2021-05-26 17:24:02 -0300

    Utilizando Repository

--- forum/src/main/java/br/com/ojc/forum/Controller/TopicosController.java
+++ forum/src/main/java/br/com/ojc/forum/Controller/TopicosController.java
@@ -3,22 +3,25 @@ package br.com.ojc.forum.Controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.com.ojc.forum.Controller.dto.TopicoDto;
-import br.com.ojc.forum.modelo.Curso;
 import br.com.ojc.forum.modelo.Topico;
+import br.com.ojc.forum.repository.TopicoRepository;
 
 @Controller
 public class TopicosController {
 
+	@Autowired
+	private TopicoRepository topicoRepository;
+	
 	@RequestMapping("/topicos")
 	@ResponseBody
 	public List<TopicoDto> lista(){
-		Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
-		
-		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+		List<Topico> topicos = topicoRepository.findAll();
+		return TopicoDto.converter(topicos);
 	}
 }
