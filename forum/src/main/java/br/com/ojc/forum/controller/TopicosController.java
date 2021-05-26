2d8023342d43a5e0f94759c12ed186c3b29203b7
oswaldo <oswaldo.jose.c@hotmail.com>
2021-05-26 18:11:56 -0300

    Boas práticas no cadastro

--- forum/src/main/java/br/com/ojc/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/ojc/forum/controller/TopicosController.java
@@ -1,14 +1,17 @@
 package br.com.ojc.forum.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.ojc.forum.controller.dto.TopicoDto;
 import br.com.ojc.forum.controller.form.TopicoForm;
@@ -39,8 +42,11 @@ public class TopicosController {
 	}
 	
 	@PostMapping
-	public void cadastrar(@RequestBody TopicoForm form) {
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
+		
+		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+		return ResponseEntity.created(uri).body(new TopicoDto(topico));
 	}
 }
