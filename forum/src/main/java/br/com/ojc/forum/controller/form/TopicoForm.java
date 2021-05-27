5a86e99e4bd50037b1a97f74cdc8e4ccdda2a454
oswaldo <oswaldo.jose.c@hotmail.com>
2021-05-27 17:16:04 -0300

    Validações com Bean Validation

--- forum/src/main/java/br/com/ojc/forum/controller/form/TopicoForm.java
+++ forum/src/main/java/br/com/ojc/forum/controller/form/TopicoForm.java
@@ -1,13 +1,21 @@
 package br.com.ojc.forum.controller.form;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
 import br.com.ojc.forum.modelo.Curso;
 import br.com.ojc.forum.modelo.Topico;
 import br.com.ojc.forum.repository.CursoRepository;
 
 public class TopicoForm {
 
+	@NotNull @NotEmpty @Length(min = 5)
 	private String titulo;
+	@NotNull @NotEmpty @Length(min = 10)
 	private String mensagem;
+	@NotNull @NotEmpty
 	private String nomeCurso;
 
 	public String getTitulo() {
