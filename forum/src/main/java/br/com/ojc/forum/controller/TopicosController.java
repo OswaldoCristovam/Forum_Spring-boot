84056652d69bf7f4c152bc105b503d8ee7d7cbfd
oswaldo <oswaldo.jose.c@hotmail.com>
2021-05-27 18:12:39 -0300

    Detalhando tópicos, pesquisando por ID

--- forum/src/main/java/br/com/ojc/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/ojc/forum/controller/TopicosController.java
@@ -2,6 +2,7 @@ package br.com.ojc.forum.controller;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -9,12 +10,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.ojc.forum.controller.dto.DetalhesDoTopicoDto;
 import br.com.ojc.forum.controller.dto.TopicoDto;
 import br.com.ojc.forum.controller.form.TopicoForm;
 import br.com.ojc.forum.modelo.Topico;
@@ -50,5 +53,12 @@ public class TopicosController {
 		
 		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
 		return ResponseEntity.created(uri).body(new TopicoDto(topico));
+	}
+	
+	@GetMapping("/{id}")
+	@ResponseBody
+	public DetalhesDoTopicoDto detalhado(@PathVariable Long id) {
+		Topico topico = topicoRepository.getOne(id);
+		return new DetalhesDoTopicoDto(topico);
 	}
 }
