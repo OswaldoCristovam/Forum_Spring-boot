d9801528b99d86f5a542c3616c4e4ab3e69b176f
oswaldo <oswaldo.jose.c@hotmail.com>
2021-05-29 09:53:23 -0300

    Paginação

--- forum/src/main/java/br/com/ojc/forum/repository/TopicoRepository.java
+++ forum/src/main/java/br/com/ojc/forum/repository/TopicoRepository.java
@@ -1,7 +1,7 @@
 package br.com.ojc.forum.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.ojc.forum.modelo.Topico;
@@ -9,6 +9,6 @@ import br.com.ojc.forum.modelo.Topico;
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
 	//Em casos que uma coluna tenha o nome igual a entidade+coluna basta separa a entidade da coluna com uma "_" 
-	List<Topico> findByCursoNome(String nomeCurso);
+	Page<Topico> findByCursoNome(String nomeCurso, Pageable paginacao);
 	
 }
