d21bb274bc59660a78a7ac53841c604f2136ebeb
oswaldo <oswaldo.jose.c@hotmail.com>
2021-05-29 11:28:18 -0300

    Autenticando o cliente via Spring Security

--- forum/src/main/java/br/com/ojc/forum/config/security/AutenticacaoViaTokenFilter.java
+++ forum/src/main/java/br/com/ojc/forum/config/security/AutenticacaoViaTokenFilter.java
@@ -7,15 +7,22 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import br.com.ojc.forum.modelo.Usuario;
+import br.com.ojc.forum.repository.UsuarioRepository;
 
 public class AutenticacaoViaTokenFilter extends OncePerRequestFilter {
 
 	private TokenService tokenService;
 	
+	private UsuarioRepository usuarioRepository;
 	
-	public AutenticacaoViaTokenFilter(TokenService tokenService) {
+	public AutenticacaoViaTokenFilter(TokenService tokenService, UsuarioRepository usuarioRepository) {
 		this.tokenService = tokenService;
+		this.usuarioRepository = usuarioRepository;
 	}
 	
 	@Override
@@ -24,9 +31,19 @@ public class AutenticacaoViaTokenFilter extends OncePerRequestFilter {
 
 		String token = recuperarToken(request);
 		boolean valido = tokenService.isTokenValido(token);
-		System.out.println(valido);
+		if(valido) {
+			autenticarCliente(token);
+		}
 		filterChain.doFilter(request, response);
 	}
+	
+	private void autenticarCliente(String token) {
+		long idUsuario = tokenService.getIdUsuario(token);
+		Usuario usuario = usuarioRepository.findById(idUsuario).get();
+		UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+	}
+
 	
 	private String recuperarToken(HttpServletRequest request) {
 		String token = request.getHeader("Authorization");
--- forum/src/main/java/br/com/ojc/forum/config/security/TokenService.java
+++ forum/src/main/java/br/com/ojc/forum/config/security/TokenService.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import br.com.ojc.forum.modelo.Usuario;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -36,5 +37,11 @@ public class TokenService {
 		} catch (Exception e) {
 			return false;
 		}
+	}
+
+	public long getIdUsuario(String token) {
+		Claims claims = Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token).getBody();
+		Long id = Long.parseLong(claims.getSubject());
+		return id;
 	}
 }
